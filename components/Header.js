b2e6a85be712129e996aa97b29d0af4d67337841
unknown <marcinoles87@gmail.com>
2022-09-30 17:49:27 +0200

    add components

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <div className='header'>
+            <h1>Welcome</h1>
+            <h2>To Our Site</h2>
+        </div>
+     );
+}
+ 
+export default Header;
