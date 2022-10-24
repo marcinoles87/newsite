c3e41ec5c746160e57dc10a018bca2c2351de165
unknown <marcinoles87@gmail.com>
2022-10-24 15:51:52 +0200

    new css

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,5 @@
 import React  from "react";
+
 import ProductStyle from '../components/ProductStyle.css';
 
 
@@ -7,9 +8,16 @@ const Product = () => {
         <div className="product">
             <h1> Our Products </h1>
             
-            <div className="product_item">
+            <div className="product_item1">
                 <h1>Product 1</h1>
+            </div>
 
+            <div className="product_item1">
+                <h1>Product 2</h1>
+            </div>
+
+            <div className="product_item1">
+                <h1>Product </h1>
             </div>
 
         </div>
