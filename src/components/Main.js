86e89c562fe1121b8131453df2ac8fec15801287
unknown <marcinoles87@gmail.com>
2022-11-14 11:08:25 +0100

    work with form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,30 @@
-import React  from "react";
+import React , {useState} from "react";
 import MainStyle from './MainStyle.css'
 
 const Main = () => {
+    const [username , setUserName] = React.useState('');
+
+ 
+
+  
+
+ 
     return ( 
         <div className="main">
             <h1> Contact Us !</h1>
 
             <form className="contact">
                 <label>Your name :</label>
-            <input type="text" className="contactName" placeholder="name"></input>
+            <input type="text" className="contactName" placeholder="name" name="username" value={username} ></input>
+                <label> Trip Date :</label>
             <input type="date" className="contactDate" placeholder="date"></input>
+                <label> Chosse Destiny :</label>
             <input className="contactDestiny" placeholder="chosse destiny"></input>
-            <textarea className="contactMessage"></textarea>
-            <button className="buttonSubmit" type="submit">Send</button>
+                <label> Your Messege :</label>
+            <textarea className="contactMessage"> </textarea>
+            <button className="buttonSubmit" type="submit" onClick={ () => setUserName(username + ' new')}>Send</button>
+           
+
             </form>
             
 
